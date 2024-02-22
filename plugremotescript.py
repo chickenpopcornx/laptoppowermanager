7350a79e8c23fe75f0b01f6ceb2c81be2c1f3bf9
likamii <nevlashkin@yandex.ru>
2024-02-22 16:55:19 +0800

    Finished main part of the script

--- plugremotescript.py
+++ plugremotescript.py
@@ -1,19 +1,55 @@
-#importing package
+from ctypes import Structure, wintypes, POINTER, windll, pointer, WinError
+import time
+
 import tinytuya
 
-#setting variable with information about device
-d = tinytuya.OutletDevice("your device id here", "device ip adress", "your device localkey here")
+import tkinter
+from tkinter import messagebox
 
-#printing info about this device(d)
-print(d)
+def batteryCharge():
+   """ АККУМУЛЯТОРНАЯ ФУНКЦИЯ """
+   class SYSTEM_POWER_STATUS(Structure):
+       _fields_ = [
+           ("ACLineStatus",       wintypes.BYTE),
+           ("BatteryFlag",        wintypes.BYTE),
+           ("BatteryLifePercent", wintypes.BYTE),
+           ]
 
-#inserting the value into the "status" variable
-print("Write On or Off:")
-status = input()
+   SYSTEM_POWER_STATUS_P = POINTER(SYSTEM_POWER_STATUS)
 
-#cheking the "status" variable value and doing function, which depends on its value
-if status == "On":
-    d.turn_on()
-else:
-    d.turn_off()
-end
+   GetSystemPowerStatus          = windll.kernel32.GetSystemPowerStatus
+   GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
+   GetSystemPowerStatus.restype  = wintypes.BOOL
+
+   status = SYSTEM_POWER_STATUS()
+   if not GetSystemPowerStatus(pointer(status)):
+      raise WinError()
+
+   charging, charge = status.ACLineStatus, status.BatteryLifePercent
+   return (charging, charge)
+
+device = tinytuya.OutletDevice("bf4a73ac7d64bb3cbfh7f5", "Auto", "8*mf8_v{j)Q00~Nj")
+
+def batteryStatus():
+   """ СТАТУС ФУНКЦИЯ """
+   chargingCharge = batteryCharge()
+   charging       = chargingCharge[0]
+   charge         = chargingCharge[1]
+
+   if charging == 0:
+      if charge <= 40:
+         device.turn_on()
+   elif charging == 1:
+      if charge >= 70:
+         device.turn_off()
+   else:
+      print("\nЭта система работает только на ноутбуках.")
+
+
+#root = tkinter.Tk()
+#messagebox.showinfo("Информация","Полезная информация")
+
+# ФУНКЦИЯ ВЫЗОВА
+while True:
+  batteryStatus()
+  time.sleep(6)
